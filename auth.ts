2fd4b2aa83946d2588e628c53fb0280c47bb25f7
wangjun <jun.wangj@me.com>
2024-03-05 22:45:01 +0800

    change credentials login

--- auth.ts
+++ auth.ts
@@ -1,5 +1,5 @@
 import NextAuth, { type DefaultSession } from 'next-auth'
-import GitHub from 'next-auth/providers/github'
+import CredentialsProvider from "next-auth/providers/credentials";
 
 declare module 'next-auth' {
   interface Session {
@@ -14,7 +14,35 @@ export const {
   handlers: { GET, POST },
   auth
 } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    CredentialsProvider({
+      id: "Credentials",
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "Jun", email: "wangjundot@gmail.com" }
+
+        if (credentials?.username == "wangjun" && credentials.password == "hello1234") {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ],
   callbacks: {
     jwt({ token, profile }) {
       if (profile) {
@@ -33,7 +61,7 @@ export const {
       return !!auth?.user // this ensures there is a logged in user for -every- request
     }
   },
-  pages: {
-    signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
-  }
+  // pages: {
+  //   signIn: '/sign-in' // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
+  // }
 })
